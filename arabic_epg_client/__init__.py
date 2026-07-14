f21e3b9614e625c3b0f690981e741db6d03dd4c4
wwwgoper77-wq <www.goper77@gmail.com>
2026-07-14 23:57:12 +0300

    Delete arabic_epg_client directory

--- arabic_epg_client/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Arabic EPG Translator Commercial Plugin
-# Professional Edition with Hardware Binding and Remote Licensing
-# (c) 2026 EPG-PRO Ltd. All rights reserved.
-
-__version__ = "2.1.0-commercial"
-__author__ = "EPG-PRO Team"
